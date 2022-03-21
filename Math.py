b3bdcc993f5434560144e7e6ed2790db9ff0d493
dmlco1 <dmlco1@iscte-iul.pt>
2022-03-21 21:19:47 +0000

    DresMax corrigido

---
+++ Math.py
@@ -0,0 +1,18 @@
+import math
+
+penalidadeMax = 1.5
+
+fatorMeritoMax = math.sqrt(10**(penalidadeMax/5)-1)/8
+
+ritmoBinario = 13e9
+lambda0 = 1545.32e-9
+c = 2.9979e8
+
+numerador = fatorMeritoMax * (2*math.pi) * c
+denominador = (ritmoBinario**2) * (lambda0**2)
+
+# DresMax em s/m = 0.582039 ...
+# DresMax em ps/nm = 582.039 ...
+dResMax = (numerador)/(denominador)
+
+
