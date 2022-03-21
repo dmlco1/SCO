200d459107f3197860406f2adb44b67817984c69
dmlco1 <dmlco1@iscte-iul.pt>
2022-03-21 21:20:38 +0000

    Menos confuso

--- Math.py
+++ Math.py
@@ -2,17 +2,12 @@ import math
 
 penalidadeMax = 1.5
 
-fatorMeritoMax = math.sqrt(10**(penalidadeMax/5)-1)/8
+fatorMeritoMax = math.sqrt(10 ** (penalidadeMax / 5) - 1) / 8
 
 ritmoBinario = 13e9
 lambda0 = 1545.32e-9
 c = 2.9979e8
 
-numerador = fatorMeritoMax * (2*math.pi) * c
-denominador = (ritmoBinario**2) * (lambda0**2)
-
 # DresMax em s/m = 0.582039 ...
 # DresMax em ps/nm = 582.039 ...
-dResMax = (numerador)/(denominador)
-
-
+dResMax = (fatorMeritoMax * (2 * math.pi) * c) / ((ritmoBinario ** 2) * (lambda0 ** 2))
