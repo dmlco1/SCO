07a642fb3bb2034498e8ead65e900b8177e8d61b
dmlco1 <dmlco1@iscte-iul.pt>
2022-03-21 21:01:44 +0000

    Remover ficheiros desnecessários

--- MaxDispertion.py
+++ MaxDispertion.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+import math
 
 ssmfDispertion = []
 dcfDispertion = []
-
+"""
 with open('DCMs.csv', 'r') as file:
     for line in file:
         if 'Dispersão SSMF' in line:
@@ -17,12 +18,24 @@ with open('DCMs.csv', 'r') as file:
         ssmfDispertion.append(float(i))
     for i in dcfArray:
         dcfDispertion.append(float(i))
+"""
+with open('DCMs.csv', 'r') as file:
+    for line in file:
+        if 'Dispersão SSMF' in line:
+            newLine = line.replace(',', '.')
+            ssmfDispertion = [float(i) for i in newLine.split(';')[1:6]]
 
-    print(ssmfDispertion)
-    print(dcfDispertion)
+        if 'Dispersão DCF Ojetivo' in line:
+            newLine = line.replace(',', '.')
+            dcfDispertion = [float(i) for i in newLine.split(';')[1:6]]
 
-    x = [0, 72, 72, 121, 121, 187, 187, 281, 281, 364, 364]
+    #for i in ssmfArray:
+    #    ssmfDispertion.append(float(i))
+    #for i in dcfArray:
+    #    dcfDispertion.append(float(i))
 
+    x1 = [0, 72, 72, 121, 121, 187, 187, 281, 281, 364, 364]
+    """
     step0 = ssmfDispertion[0]
     step1 = ssmfDispertion[0] + dcfDispertion[0]
     step2 = step1 + ssmfDispertion[1]
@@ -33,15 +46,34 @@ with open('DCMs.csv', 'r') as file:
     step7 = step6 + dcfDispertion[3]
     step8 = step7 + ssmfDispertion[4]
     step9 = step8 + dcfDispertion[4]
+    """
 
-    y = [0, step0, step1, step2, step3, step4, step5, step6, step7, step8, step9]
+    step = [0]*10
+    for i in range(10):
+        if i == 0:
+            print(step[0])
+            step[i] = ssmfDispertion[0]
+            print(step[i])
+        elif i % 2 == 0:
+            step[i] = step[i - 1] + ssmfDispertion[math.floor(i / 2)]
+            print(step[i])
+        elif i % 2 != 0:
+            step[i] = step[i - 1] + dcfDispertion[math.floor(i / 2)]
+            print(step[i])
 
-    plt.plot(x, y, color='#a14442')
+    # y1 = [0, step0, step1, step2, step3, step4, step5, step6, step7, step8, step9]
+
+    y2 = [0, ssmfDispertion[0], 0, ssmfDispertion[1], 0, ssmfDispertion[2], 0, ssmfDispertion[3], 0, ssmfDispertion[4],
+          0]
+    step.insert(0,0)
+    #plt.plot(x1, y1, color='red')
+    plt.plot(x1, step, color='red', label='Máxima Dispersão Residual')
+    plt.plot(x1, y2, color='green',linestyle="--", label='Compensação Perfeita de Dispersão')
+    plt.legend(loc='upper left')
     plt.title('Regra da Máxima Dispersão Residual', color='#2362b7')
     plt.ylabel('Dispersão Acumulada [ps/nm]', color='#1e5cac')
     plt.xlabel('Distância ao Emissor [km]', color='#1e5cac')
     plt.show()
-
 
 '''
 x = [0, 72, 72, 121, 121, 187, 187, 281, 281, 364, 364]
