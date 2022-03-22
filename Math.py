b3906c200d35729a32b02618233177454b635040
rfgoo <rfgoo@iscte-iul.pt>
2022-03-22 13:55:05 +0000

    uma quantidade demasiado grande de informação adicional

--- Math.py
+++ Math.py
@@ -1,33 +1,26 @@
 import math
 from numpy import vstack, ones
 from numpy.linalg import lstsq
+import data
 
-penalidadeMax = 1.5  # dB
 
-fatorMeritoMax = math.sqrt(10 ** (penalidadeMax / 5) - 1) / 8
+# dlambda para a nossa frequencia
+Dlambda = ((data.So/4) * (data.lambda1 - (data.lambda0**4/data.lambda1**3)))
+Dlambda_psnmkm = Dlambda *(1e12/(1e9*1e-3))
+print(f"Dlambda {Dlambda}")
+print(f"Dlambda_psnmkm {Dlambda_psnmkm}")
 
-ritmoBinario = 13e9  # bps
-lambda0 = 1545.32e-9  # m
-c = 2.9979e8  # m/s
+#fator de mérito
+fatorMeritoMax = math.sqrt(10 ** (data.penalidadeMax / 5) - 1) / 8
+print(f"Fator de mérito {fatorMeritoMax}")
 
+#Dres máximo
 # DresMax em s/m = 0.582039 ...
 # DresMax em ps/nm = 582.039 ...
-DresMax = (fatorMeritoMax * (2 * math.pi) * c) / ((ritmoBinario ** 2) * (lambda0 ** 2))  # s/m
+DresMax = (fatorMeritoMax * (2 * math.pi) * data.c) / ((data.ritmoBinario ** 2) * (data.lambda1 ** 2))  # s/m
+print(f"DresMax {DresMax}")
 
-# x = disperção
-
-vals = [
-      [(-48, 1530), (-51, 1550)],
-      [(-159, 1530), (-171, 1550)],
-      [(-320, 1530), (-343, 1550)],
-      [(-479, 1530), (-514, 1550)],
-      [(-639, 1530), (-685, 1550)],
-      [(-959, 1530), (-1028, 1550)],
-      [(-1278, 1530), (-1371, 1550)],
-      [(-1598, 1530), (-1713, 1550)]
-      ]
-
-
+# interpolação
 def line_eq(pontos):
     x_coords, y_coords = zip(*pontos)
     A = vstack([x_coords, ones(len(x_coords))]).T
@@ -35,12 +28,25 @@ def line_eq(pontos):
     # print("Line Solution is y = {m}x + {c}".format(m=m, c=c))
     return m, c
 
+
 def interpolation_values():
     results = []
-    for i in vals:
-        print(i)
+    for i in data.val_data_sheet:
+        # print(i)
         declive, b = line_eq(i)
-        x = ((lambda0*10**9)-b)/declive
+        x = ((data.lambda1*10**9)-b)/declive
         results.append(x)
-        print(x)
+    # print(results)
     return results
+
+interpolation_data_sheet = interpolation_values()
+
+# Ddcf dos links (objetivo)
+ddcf_obj = [(DresMax/len(data.lengths_worst_case))-(Dlambda_psnmkm*l) for l in data.lengths_worst_case]
+print(f"\nDCF dos links (objetivo) {ddcf_obj}\n")
+
+# discpersão do DCF escolhido
+escolhido = [interpolation_data_sheet[5], interpolation_data_sheet[4], interpolation_data_sheet[5], interpolation_data_sheet[6], interpolation_data_sheet[6]]
+print(f"DCF escolhido {escolhido}")
+
+
