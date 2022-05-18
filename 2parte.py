4ac147ed5e0af90300c012fef6f628a52f8a613d
dmlco1 <dmlco1@iscte-iul.pt>
2022-05-18 17:23:18 +0100

    coisas da aula

--- 2parte.py
+++ 2parte.py
@@ -1,6 +1,9 @@
+# import math
 import math
+
 from tabulate import tabulate
 import data2 as d2
+import data as d1
 
 # emissores banda pelo critério de nyquist
 banda = d2.Rb_canal / 2 * 1.20
@@ -11,38 +14,46 @@ print("número de canais: " + str(ch))
 
 figuras_merito = []
 emissores = [i for i in d2.emissor]
+recetores = [i for i in d2.recetor]
 awg = [i for i in d2.awgs]
 
 for i in d2.emissor:
     if i[3] >= banda:
-        potencia = 10 * math.log10(i[1])
-        rext = 1 / i[2]
-        fm = potencia - 10 * math.log10(abs((rext + 1) / (rext - 1)))
-        print(i[0] + " " + str(fm))
         for j in awg:
             if 2 * d2.Rb_canal + 2 * i[5] <= j[2] <= j[1]:
-                print(j[0] + " pode ser usado")
+                # print(j[0] + " pode ser usado")
+                pass
             else:
                 emissores.remove(i)
-
     else:
         emissores.remove(i)
+print(emissores)
+
+for i in d2.recetor:
+    if i[5] < banda:
+        recetores.remove(i)
+print(recetores)
+
+combinacoes = [(x, y) for x in emissores for y in recetores]
+print("Combinações")
+for i in combinacoes:
+    print(i)
+
 
 canais_binf = [d2.canal5_inf + (i - 5) * d2.awgs[0][1] for i in range(1, 6)]
-print(f"banda inferior: {canais_binf}")
+# print(f"banda inferior: {canais_binf}")
 canais_bsup = [d2.canal5_sup - (i - 5) * d2.awgs[0][1] for i in range(1, 6)]
-print(f"banda superior: {canais_bsup}")
+# print(f"banda superior: {canais_bsup}")
 
 tab = []
 
 tab.insert(0, ["", "Banda inferior", "Banda central", "Banda superior"])
 tab.insert(1, ["Id canal", "𝒗 [THz]", "𝒗 [THz]", "𝒗 [THz]"])
 
-for i in range(0, 5):
-    tab.insert(i+2, [f"{i+2}", f"{canais_binf[i]}", f"-", f"{canais_bsup[i]}"])
-
-
-# tab.insert(i+2, [f"{i+2}", f"{canais_binf[i]}", f"-", f"{canais_bsup[i]}"] for i in range(0, 5))
-
+for i in range(2, 7):
+    tab.insert(i, [f"{i-1}", f"{canais_binf[i-2]}", f"-", f"{canais_bsup[i-2]}"])
 
 print(tabulate(tab, tablefmt="fancy_grid", stralign="center"))
+
+n_juntas = [math.floor(i/2.5) - 1 for i in d1.lengths_section]
+print(n_juntas)
--- data2.py
+++ data2.py
@@ -1,4 +1,6 @@
 import data
+import math
+
 # ritmo binario de canal em bits
 Rb_canal = 13e9
 # ritmo binario total em bits
@@ -13,24 +15,39 @@ emissor = [['A', 16, 8, 12e9, 10, 4],
            ['F', 3, 16, 2e9, 1, 4.5]]
 
 # nome, eff quantica(%, NEP (pW/Hz^1/2), ordem do filtro butterworth, parametro de sobrecarga(dBm),largura de banda(a -3dB)(GHz)
-recetor = [['A', 75, 15, 4, -4, 9],
-           ['B', 65, 20, 5, -5, 11],
-           ['C', 55, 25, 5, -6, 13],
-           ['D', 50, 2, 2, 1, 2],
-           ['E', 75, 4, 2, 0, 3],
-           ['F', 65, 6, 3, -1, 4],
-           ['G', 55, 8, 3, -2, 4.5],
-           ['H', 50, 10, 4, -3, 6.5]]
-#nome,espaçamento entre canais, banda a -3dB de AWG
-#como so temos 5 canais vamos usar o AWG50
-awgs=[['AWG50', 50e9, 35e9],
-   ['AWG100', 100e9, 75e9]]
-#banda inferior (lambda min, freq max)
-banda_inf=[1543e-9, data.c/1543e-9]
-#canal5 em teras
-canal5_inf=194.25e12
-#banda superior (lambda max, freq min)
-banda_sup=[1545.79e-9, data.c/1545.79e-9]
+recetor = [['A', 75, 15, 4, -4, 9e9],
+           ['B', 65, 20, 5, -5, 11e9],
+           ['C', 55, 25, 5, -6, 13e9],
+           ['D', 50, 2, 2, 1, 2e9],
+           ['E', 75, 4, 2, 0, 3e9],
+           ['F', 65, 6, 3, -1, 4e9],
+           ['G', 55, 8, 3, -2, 4.5e9],
+           ['H', 50, 10, 4, -3, 6.5e9]]
+# nome,espaçamento entre canais, banda a -3dB de AWG
+# como so temos 5 canais vamos usar o AWG50
+awgs = [['AWG50', 50e9, 35e9],
+        ['AWG100', 100e9, 75e9]]
+# banda inferior (lambda min, freq max)
+banda_inf = [1543e-9, data.c / 1543e-9]
+# canal5 em teras
+canal5_inf = 194.25e12
+# banda superior (lambda max, freq min)
+banda_sup = [1545.79e-9, data.c / 1545.79e-9]
 """193.90-(espaçamentoCH/2)"""
-#canal 5 em teras
-canal5_sup=193.95e12
+# canal 5 em teras
+canal5_sup = 193.95e12
+
+
+def butval(ordem):
+    return (math.py / (2 * ordem)) / math.sin(math.py / (2 * ordem))
+
+n2 = 2.6e-20 #mW
+Aeff = 80 #micrometro^2
+
+#pior caso... lambda menor
+def gamma(comp_onda):
+    return (2*math.pi*n2)/(comp_onda*Aeff)
+
+# ver alfa
+def leff(alfa_npm):
+    return (1-math.exp(-alfa_npm))/alfa_npm
